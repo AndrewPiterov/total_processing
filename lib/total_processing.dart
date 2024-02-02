8c07a4e926bc5ee6860cd86d569e946ba43fe364
ZAKI MUBAROK <kakzaki@gmail.com>
2024-02-02 14:54:33 +0700

    chore(flutter): initial code

--- lib/total_processing.dart
+++ lib/total_processing.dart
@@ -1,8 +1,35 @@
-
-import 'total_processing_platform_interface.dart';
+import 'package:flutter/services.dart';
 
 class TotalProcessing {
-  Future<String?> getPlatformVersion() {
-    return TotalProcessingPlatform.instance.getPlatformVersion();
+  final methodChannel = const MethodChannel('total_processing');
+
+  Future<String?> requestCheckoutId() {
+    return methodChannel.invokeMethod(
+      'requestCheckoutId',
+    );
   }
+
+  void _checkoutSettings() {
+    methodChannel.invokeMethod(
+      'checkoutSettings',
+    );
+  }
+
+  void startCheckout() {
+    methodChannel.invokeMethod(
+      'startCheckout',
+    );
+  }
+
+  Future<String?> requestPaymentStatus() {
+    return methodChannel.invokeMethod(
+      'requestPaymentStatus',
+    );
+  }
+
+  final EventChannel _handleCheckoutResult =
+      const EventChannel('handleCheckoutResult');
+  Stream get handleCheckoutResultStream => _handleCheckoutResult
+      .receiveBroadcastStream(_handleCheckoutResult.name)
+      .cast();
 }
