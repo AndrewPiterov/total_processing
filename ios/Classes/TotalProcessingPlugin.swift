6e9555edef9a567680e9374b9b3ffc9a3ebd771f
Zaki Mubarok <31821107+alviazirin@users.noreply.github.com>
2024-02-09 15:04:00 +0700

    chore(ios): implement ios sdk

--- ios/Classes/TotalProcessingPlugin.swift
+++ ios/Classes/TotalProcessingPlugin.swift
@@ -23,6 +23,8 @@ public class TotalProcessingPlugin: NSObject, FlutterPlugin, FlutterStreamHandle
     let channel = FlutterMethodChannel(name: "total_processing", binaryMessenger: registrar.messenger())
     let instance = TotalProcessingPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
+    let handleCheckoutResultChannel = FlutterEventChannel(name: handleCheckoutResultEvent, binaryMessenger: registrar.messenger())
+    handleCheckoutResultChannel.setStreamHandler(instance)
   }
     
 
@@ -55,43 +57,33 @@ public class TotalProcessingPlugin: NSObject, FlutterPlugin, FlutterStreamHandle
         var item: [String: Any?] = [:]
         // Since version 2.13.0
         checkoutProvider?.presentCheckout(forSubmittingTransactionCompletionHandler: { (transaction, error) in
-            if transaction?.type == .synchronous {
-                // If a transaction is synchronous, just request the payment status
-                // You can use transaction.resourcePath or just checkout ID to do it
-                // Error is no more an decisive factor for transaction termination
-                
-//                item["isErrored"] = result.isErrored
-//                var paymentError: [String: Any?] = [:]
-//                paymentError["errorCode"] = result.paymentError?.errorCode.map { String($0) }
-//                paymentError["errorInfo"] = result.paymentError?.errorInfo
-//                paymentError["errorMessage"] = result.paymentError?.errorMessage
-//                item["paymentError"] = paymentError
-          
-//
-//                var transaction: [String: Any?] = [:]
-//                transaction["transactionType"] = result.transaction?.transactionType.map { String($0) }
-//                var paymentParams: [String: Any?] = [:]
-//                paymentParams["checkoutId"] = result.transaction?.paymentParams?.checkoutId
-//                paymentParams["paymentBrand"] = result.transaction?.paymentParams?.paymentBrand
-//                paymentParams["shopperResultUrl"] = result.transaction?.paymentParams?.shopperResultUrl
-//                transaction["paymentParams"] = paymentParams
-//                transaction["brandSpecificInfo"] = result.transaction?.brandSpecificInfo.map { $0.description }
-//                transaction["redirectUrl"] = result.transaction?.redirectUrl.map { $0.description }
-//                transaction["threeDS2Info"] = result.transaction?.threeDS2Info.map { $0.description }
-//                transaction["threeDS2MethodRedirectUrl"] = result.transaction?.threeDS2MethodRedirectUrl.map { $0.description }
-//                transaction["yooKassaInfo"] = result.transaction?.yooKassaInfo.map { $0.description }
-//                item["transaction"] = transaction
-                if ((transaction?.resourcePath) != nil) {
-                     // get the payment status using the resourcePath
-                    item["resourcePath"] = transaction?.resourcePath
-                }
-            } else if transaction?.type == .asynchronous {
-                // The SDK opens transaction.redirectUrl in a browser
-                // See 'Asynchronous Payments' guide for more details
+            guard let transaction = transaction else {
+                item["isErrored"] = true
+                var paymentError: [String: Any?] = [:]
+                paymentError["errorCode"] = "401"
+                paymentError["errorInfo"] = "error"
+                paymentError["errorMessage"] = error?.localizedDescription
+                item["paymentError"] = paymentError
+                self.handleCheckoutResultSink?(item)
+                return
             }
+            
+            var trans: [String: Any?] = [:]
+            if transaction.type == .synchronous {
+                trans["transactionType"] = "synchronous"
+            } else if transaction.type == .asynchronous {
+                trans["transactionType"] = "asynchronous"
+            }
+            var paymentParams: [String: Any?] = [:]
+            paymentParams["checkoutId"] = checkoutID
+            trans["paymentParams"] = paymentParams
+            trans["desc"] = transaction.description
+            item["transaction"] = trans
+            self.handleCheckoutResultSink?(item)
         }, cancelHandler: {
             // Executed if the shopper closes the payment page prematurely
             item["isCanceled"] = true
+            self.handleCheckoutResultSink?(item)
         })
         
     }
